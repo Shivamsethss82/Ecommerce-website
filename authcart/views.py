c23468bf684ff8550c7239deb81fcc74ac6ad6d8
SHIVAM <shivamsethss82@gmail.com>
2023-09-14 01:17:42 +0530

    Add otp section

--- authcart/views.py
+++ authcart/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.models import User
+from django.views.generic import View
 from django.contrib import messages
-from django.conf.urls.static import static
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+from .utils import TokenGenerator,generate_token
+from django.utils.encoding import force_bytes , force_str , DjangoUnicodeDecodeError
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 
 # Create your views here.
@@ -24,7 +30,33 @@ def signup(request):
         user = User.objects.create_user(email,email,password)
         user.is_active=False
         user.save()
+        email_subject="Activate Your Account"
+        message=render_to_string('authentication/activate.html',{
+            'user':user,
+            'domain':'127.0.0.1:8000',
+            'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+            'token':generate_token.make_token(user)
+
+        })
+        email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
+        email_message.send()
+        messages.success(request,f"Activate Your Account by clicking the link in your gmail {message}")
+        return redirect('/auth/login/')
     return render(request, 'authentication/signup.html')
+
+class ActivateAccountView(View):
+    def get(self,request,uidb64,token):
+        try:
+            uid=force_str(urlsafe_base64_decode(uidb64))
+            user=User.objects.get(pk=uid)
+        except Exception as identifier:
+            user=None
+        if user is not None and generate_token.check_token(user,token):
+            user.is_active=True
+            user.save()
+            messages.info(request,"Account Activated Successfully")
+            return redirect('/auth/login')
+        return render(request,'activatefail.html')
 
 
 def handlelogin(request):
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -114,6 +114,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+## Sending Emails
+
+
+EMAIL_HOST = 'smtpout.secureserver.net'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
